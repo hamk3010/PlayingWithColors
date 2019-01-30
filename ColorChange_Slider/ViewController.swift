8dbba93a74c22a35eaf77f322bf3864e789bc6e4
Hamza Khan <hamza.khan.9440@gmail.com>
2019-01-29 23:41:45 -0800

    playing with colors completed

--- ColorChange_Slider/ViewController.swift
+++ ColorChange_Slider/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent(_ sender: AnyObject) {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
